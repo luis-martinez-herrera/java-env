5bfcfa3d899babe791af673c668fcb976e75d6be
luis martinez <luismartinez@outlook.com.pe>
2022-06-18 15:58:06 -0500

    add env file

---
+++ myenvvars.sh
@@ -0,0 +1,19 @@
+#export JAVA_HOME=/home/dev/libs/jdk-11.0.13
+#export PATH=$PATH:$JAVA_HOME/bin
+
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+
+export MAVEN_HOME=/home/dev/libs/apache-maven-3.8.5
+export PATH=$PATH:$MAVEN_HOME/bin
+
+export GRADLE_HOME=/home/dev/libs/gradle-3.5.1
+export PATH=$PATH:$GRADLE_HOME/bin
+
+export GCLOUD_HOME=/home/dev/libs/google-cloud-sdk
+export PATH=$PATH:$GCLOUD_HOME/bin
+
+export NODE_HOME=/home/dev/libs/node-v16.15.0-linux-x64
+export PATH=$PATH:$NODE_HOME/bin
+
+export DRUID_JAVA_HOME=/home/dev/libs/jdk1.8.0_321
